42f6ef949602b01c94298a9cad0ad36b6177d076
Wiratamaradit <133556756+Wiratamaradit@users.noreply.github.com>
2024-04-23 21:19:08 +0700

    UpdateLoantrans-BE

--- app/Http/Controllers/LoantransController.php
+++ app/Http/Controllers/LoantransController.php
@@ -10,19 +10,19 @@ use App\Models\installment;
 class LoantransController extends Controller
 {
     function loantransAdd(Request $request)
-    {
-        $installmentId = $request->input('installmentId');
-        $installment = installment::find($installmentId);
-        if ($installment) {
-            return response()->json(['success' => 'installment ditemukan.'], 200);
-        }
-        $base64Data = $request->input('base64_data');
+{
+    $installmentId = $request->input('installmentId');
+    $installment = installment::find($installmentId);
+
+    if (!$installment) {
+        return response()->json(['error' => 'Installment not found'], 404);
+    }
+    $base64Data = $request->input('base64_data');
+    if ($base64Data) {
         $fileData = base64_decode($base64Data);
         $fileName = 'BuktiPembayaran_' . time() . '.pdf';
         $filePath = storage_path('app/pdf_files/' . $fileName);
-
         file_put_contents($filePath, $fileData);
-
         $loantrans = new loantransaction;
         $loantrans->installmentId = $installment->id;
         $loantrans->fileName = $fileName;
@@ -30,7 +30,10 @@ class LoantransController extends Controller
         $loantrans->save();
 
         return response()->json(['message' => 'File berhasil diunggah.', 'data' => $loantrans], 200);
+    } else {
+        return response()->json(['error' => 'No file data provided'], 400);
     }
+}
 
     function loantransList()
     {
